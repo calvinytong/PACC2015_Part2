8c71544f7295708ccb82c3f1e84da0098ca52e24
calvintong <calvin@tongfamily.com>
2015-05-20 21:50:14 -0700

    Does nothing but doesn't break it

--- Pacer/AViewController.swift
+++ Pacer/AViewController.swift
@@ -4,16 +4,19 @@ import UIKit
 import Parse
 
 
-class AViewController: UIViewController {
+class AViewController: UIViewController, UITableViewDataSource{
     
     @IBOutlet weak var welcome: UILabel!
-    
     var SIZE = UIScreen.mainScreen().bounds.width; // sets width
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a 
         self.view.frame.size.width = SIZE;
+        updateUserInfo()
+        userTable.reloadData()
     }
     
     override func didReceiveMemoryWarning() {
@@ -42,5 +45,48 @@ class AViewController: UIViewController {
             self.welcome.text = PFUser.currentUser() == nil ? "" : "Hello \(PFUser.currentUser()!.username!)"
             self.labelSize(self.welcome)
         })
+        
+    }
+    
+    @IBOutlet weak var userTable: UITableView!
+    
+    var keyTable:[NSString] = []
+    //var introDict = Dictionary<String, String>()
+    var valueDict = Dictionary<NSString, NSString>()
+    
+    func updateUserInfo(){
+        var currentUserQuery = PFQuery(className: "Player")
+        var userProfile = PFUser.currentUser()!["profile"] as? PFObject
+        if (userProfile == nil){
+            keyTable.append("what")
+            valueDict.updateValue("fucking hell", forKey: "what")
+        } else {
+            for key in userProfile!.allKeys() as NSArray {
+                println(key)
+                if let keyStr = key as? NSString {
+                    keyTable.append(keyStr)
+                    valueDict.updateValue("fucking hell" as NSString, forKey: keyStr)
+                    println(keyStr)
+                }
+            }
+            keyTable.append("what2")
+            valueDict.updateValue("fucking hell2", forKey: "what2")
+            println("Done with keys")
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return keyTable.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Value2, reuseIdentifier: nil)
+        
+        let rowTitle = keyTable[indexPath.row]
+        let rowContent = valueDict[rowTitle]
+        
+        cell.textLabel?.text = "\(rowTitle):"
+        cell.detailTextLabel?.text = "\(rowContent)"
+        return cell
     }
 }
